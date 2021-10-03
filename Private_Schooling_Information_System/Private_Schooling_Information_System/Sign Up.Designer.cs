aa30ff7be86afbf203034db0882f8c8dff348467
Rudolph van Rensburg <rudolph.vanrensburg@gmail.com>
2021-10-03 19:43:39 +0200

    Initial Commit. Sign Up page
    Sign up GUI

---
+++ Private_Schooling_Information_System/Private_Schooling_Information_System/Sign Up.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Private_Schooling_Information_System
+{
+    partial class Sign_Up
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnSignUp2 = new System.Windows.Forms.Button();
+            this.txtPassword2 = new System.Windows.Forms.TextBox();
+            this.txtUserName2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtConfirmPassword = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(46, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 16);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Password:";
+            // 
+            // btnSignUp2
+            // 
+            this.btnSignUp2.Location = new System.Drawing.Point(205, 233);
+            this.btnSignUp2.Name = "btnSignUp2";
+            this.btnSignUp2.Size = new System.Drawing.Size(75, 23);
+            this.btnSignUp2.TabIndex = 11;
+            this.btnSignUp2.Text = "Sign Up";
+            this.btnSignUp2.UseVisualStyleBackColor = true;
+            // 
+            // txtPassword2
+            // 
+            this.txtPassword2.Location = new System.Drawing.Point(180, 150);
+            this.txtPassword2.Name = "txtPassword2";
+            this.txtPassword2.Size = new System.Drawing.Size(100, 20);
+            this.txtPassword2.TabIndex = 10;
+            // 
+            // txtUserName2
+            // 
+            this.txtUserName2.Location = new System.Drawing.Point(180, 113);
+            this.txtUserName2.Name = "txtUserName2";
+            this.txtUserName2.Size = new System.Drawing.Size(100, 20);
+            this.txtUserName2.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(46, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 16);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "User Name:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(106, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 33);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Sign Up";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(46, 193);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(119, 16);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Confirm Password:";
+            // 
+            // txtConfirmPassword
+            // 
+            this.txtConfirmPassword.Location = new System.Drawing.Point(180, 189);
+            this.txtConfirmPassword.Name = "txtConfirmPassword";
+            this.txtConfirmPassword.Size = new System.Drawing.Size(100, 20);
+            this.txtConfirmPassword.TabIndex = 13;
+            // 
+            // Sign_Up
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(341, 301);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtConfirmPassword);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnSignUp2);
+            this.Controls.Add(this.txtPassword2);
+            this.Controls.Add(this.txtUserName2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Sign_Up";
+            this.Text = "Sign_Up";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnSignUp2;
+        private System.Windows.Forms.TextBox txtPassword2;
+        private System.Windows.Forms.TextBox txtUserName2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtConfirmPassword;
+    }
+}
